6afbc7d3cdf1dbe66e8e06b2f8eb8df4ef1ff108
Aliaksandr Terashkou <aliaksandr.terashkou@itechart-group.com>
2022-02-06 18:08:43 +0300

    Inital project

---
+++ MyReads/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyReads
+//
+//  Created by nb-058-41b on 2/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
