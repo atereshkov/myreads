d1c38d6d2753ddf1e47ab527c30a7bc77f97466c
Alexander Tereshkov <alex.tereshkov@bk.ru>
2022-02-15 22:31:51 +0300

    Fix deprecation warning

--- MyReadsPackages/MyReadsComponentsKit/Sources/MyReadsComponentsKit/View/CircleLoadingView.swift
+++ MyReadsPackages/MyReadsComponentsKit/Sources/MyReadsComponentsKit/View/CircleLoadingView.swift
@@ -16,17 +16,17 @@ public struct CircleLoadingView: View {
                 .fill(color)
                 .frame(width: 14, height: 14)
                 .scaleEffect(shouldAnimate ? 1.0 : 0.5)
-                .animation(Animation.easeInOut(duration: 0.5).repeatForever())
+                .animation(Animation.easeInOut(duration: 0.5).repeatForever(), value: shouldAnimate)
             Circle()
                 .fill(color)
                 .frame(width: 14, height: 14)
                 .scaleEffect(shouldAnimate ? 1.0 : 0.5)
-                .animation(Animation.easeInOut(duration: 0.5).repeatForever().delay(0.3))
+                .animation(Animation.easeInOut(duration: 0.5).repeatForever().delay(0.3), value: shouldAnimate)
             Circle()
                 .fill(color)
                 .frame(width: 14, height: 14)
                 .scaleEffect(shouldAnimate ? 1.0 : 0.5)
-                .animation(Animation.easeInOut(duration: 0.5).repeatForever().delay(0.6))
+                .animation(Animation.easeInOut(duration: 0.5).repeatForever().delay(0.6), value: shouldAnimate)
         }
         .onAppear {
             self.shouldAnimate = true
