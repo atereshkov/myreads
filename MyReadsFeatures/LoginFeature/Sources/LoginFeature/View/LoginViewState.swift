e6ef63a91a7fc7c9fb0b84a92252bb865f399e3e
Alexander Tereshkov <alex.tereshkov@bk.ru>
2022-02-16 20:15:21 +0300

    Small fixes

--- MyReadsFeatures/LoginFeature/Sources/LoginFeature/View/LoginViewState.swift
+++ MyReadsFeatures/LoginFeature/Sources/LoginFeature/View/LoginViewState.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 enum LoginViewState {
     case start
     case loading
--- MyReadsPackages/AppCore/Sources/AppCore/AppBootstrap.swift
+++ MyReadsPackages/AppCore/Sources/AppCore/AppBootstrap.swift
@@ -6,6 +6,8 @@ public class AppBootstrap {
         
     }
 
+    /// Sets the initial data to the AppState object.
+    /// - Parameter state: AppState object that will be updated asynchronously
     public func boot(with state: Store<AppState>) {
         state[\.auth.isAuthorized] = true
     }
